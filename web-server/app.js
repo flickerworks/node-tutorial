e12220a859fe6e6677218f31cf18d325afa2658e
arpit.imcl@gmail.com <ac00464101@techmahindra.com>
2021-10-14 21:08:07 +0530

    add all data

---
+++ web-server/app.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const hbs = require('hbs');
+const express = require('express');
+const axios = require('axios').default;
+const app = express();
+
+const staticExpressPath = path.join(__dirname, '../public');
+// const viewsPath = path.join(__dirname, '../src');
+ const partialsPath = path.join(__dirname, '/partials');
+hbs.registerPartials(partialsPath)
+
+app.set('view engine', 'hbs')
+
+//to change handler default dir (views) to src
+// app.set('views', viewsPath) 
+
+
+//setup static directory to serve pages
+app.use(express.static(staticExpressPath)) 
+
+app.get('', function(req, res){
+    res.render('index', {
+        title: 'NodeJs Tutorial',
+        name: 'Arpit Chauhan'
+    })
+})
+
+app.get('/about', function(req, res){
+    res.render('about', {
+        title: 'About',
+        about: '"Handle bars" to render dynamic template!'
+    })
+})
+
+app.get('/users', function(req, res){
+    res.render('users', {
+        title: 'Employees',
+        about: 'List of all employees'
+    })
+})
+
+app.get('/employees', function(req, res){
+    if(!req.query.page){
+        return res.send({
+            error: 'No page index found!'
+        })
+    }
+    axios.get(`https://reqres.in/api/users?page=${req.query.page}`)
+    .then(function (response) {
+        // handle success
+        res.send(response.data.data)
+    })
+    .catch(function (error) {
+        // handle error
+        return res.send({
+            error
+        })
+    })
+    .then(function () {
+        // always executed
+    })    
+})
+
+
+app.listen('3000', function(){
+    console.log('The server has started...')
+})
